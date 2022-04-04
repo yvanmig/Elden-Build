babaeb5725d9910384146dcb69ba50c44bfc55c2
yvanmig <yvan.gimard@gmail.com>
2022-04-04 19:39:35 +0200

    wip - login forms

--- lib/layout/pages/login.dart
+++ lib/layout/pages/login.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 //import 'package:google_fonts/google_fonts.dart';
 import 'package:elden_build/layout/layout.dart';
@@ -14,42 +15,81 @@ class Login extends StatelessWidget {
       backgroundColor: Colors.black,
       body:
       Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Image(
-            height: 230,
-            image: NetworkImage('img/logo.png'),
-          ),
-          Text(
-              'Elden rinG',
-              style: TextStyle(
-                color: Colors.white,
-              )
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 75, vertical: 8),
-            child: TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                border: UnderlineInputBorder(),
-                labelText: 'Enter your username',
+        children: [
+          Column(
+            children: const [
+              Image(
+                height: 230,
+                image: NetworkImage('img/logo.png'),
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 75, vertical: 8),
-            child: TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                border: UnderlineInputBorder(),
-                labelText: 'Password',
+              Text(
+                  'Elden rinG',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40
+                  )
               ),
-            ),
+              const Text(
+                  'LogiN',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30
+                  )
+              ),
+            ],
+          ),
+          Column(
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 75, vertical: 8),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: UnderlineInputBorder(),
+                    labelText: 'Enter your username',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 75, vertical: 8),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: UnderlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const FormButton(buttonText: "LOG IN", route: "/login"),
+              RichText(
+                text: TextSpan(
+                children: [
+                  const TextSpan(
+                  text: "You don't have an account yet ?",
+                  style: TextStyle(color: Colors.white),
+                  ),
+                  TextSpan(
+                    text: ' sign in',
+                    style: const TextStyle(color: Colors.blueAccent),
+                    recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.pushNamed(context, "/register");
+                     },
+                   ),
+                  ],
+                 ),
+                )
+              ],
           ),
         ],
       ),
--- lib/layout/pages/register.dart
+++ lib/layout/pages/register.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 //import 'package:google_fonts/google_fonts.dart';
 import 'package:elden_build/layout/layout.dart';
@@ -11,8 +12,111 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body:
-      Text('REGISTER'),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            children: const [
+              Image(
+                height: 230,
+                image: NetworkImage('img/logo.png'),
+              ),
+              Text(
+                  'Elden rinG',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40
+                  )
+              ),
+              const Text(
+                  'RegisteR',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30
+                  )
+              ),
+            ],
+          ),
+          Column(
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 75, vertical: 8),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: UnderlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 75, vertical: 8),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: UnderlineInputBorder(),
+                    labelText: 'Pseudo',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 75, vertical: 8),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: UnderlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 75, vertical: 8),
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: UnderlineInputBorder(),
+                    labelText: 'Confirm password',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              FormButton(buttonText: "REGISTER", route: "/REGISTER"),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: "You already have an account ?",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    TextSpan(
+                      text: ' log in',
+                      style: const TextStyle(color: Colors.blueAccent),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.pushNamed(context, "/login");
+                        },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
 
   }
