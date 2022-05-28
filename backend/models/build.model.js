a352180c2378870d6f58716b6e6b83cee960e479
UTkraken <48678025+UTkraken@users.noreply.github.com>
2022-05-28 14:38:15 +0200

    edit builds model

--- backend/models/build.model.js
+++ backend/models/build.model.js
@@ -5,8 +5,25 @@ const buildSchema = schema({
     name: {type: String, required: true},
     image: {type: String, required: true},
     description: {type: String, required: true},
-    stat1: {type: String, required: true},
-    stat2: {type: String, required: true}
+    mainStat1: {type: String, required: true},
+    mainStat2: {type: String, required: true},
+    vigor: {type: Number, required: true}, 
+    mind: {type: Number, required: true},
+    endurence: {type: Number, required: true},
+    strength: {type: Number, required: true},
+    dexterity: {type: Number, required: true},
+    intelligence: {type: Number, required: true},
+    faith: {type: Number, required: true},
+    arcane: {type: Number, required: true},
+    weapon1: {type: Number, required: true}, 
+    weapon2: {type: Number, required: false},
+    weapon3: {type: Number, required: false},
+    spell1: {type: Number, required: true}, 
+    spell2: {type: Number, required: false},
+    spell3: {type: Number, required: false},
+    talisman1: {type: Number, required: true}, 
+    talisman2: {type: Number, required: true},
+    talisman3: {type: Number, required: true},
 });
 
 const EldenBuild = mongoose.model("builds", buildSchema);
